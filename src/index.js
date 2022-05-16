63b63294e912190b46b43c6018822d81bc564dba
Yannick PERRET <yannick.perret@emploilausanne.ch>
2022-05-16 16:01:16 +0200

    Mise en place du webpack et modification pour SCSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import './style/_settings.scss'
 
-
-
-    
-window.onload = function (){
-   
+window.scrollToPage = (id) => {
+    id.scrollIntoView({behavior: 'smooth'});
 }
+
+window.showMenuMobile = () =>{
+    const nav = document.querySelector("nav")
+    if(nav.style.display  === "flex"){
+        nav.style.display = "none";
+
+    }else {
+        nav.style.display = "flex";
+    }
+}
+
+
+/*Scroll click sur l'arrow pour remonter l'utilisateur*/
+
+window.addEventListener("scroll", () => {
+    let arrowUp = document.querySelector(".gotoTop")
+    if(window.scrollY >= 50){
+        arrowUp.style.display = "flex"
+    }
+    else{
+        arrowUp.style.display = "none"
+    }
+})
+
+
+document.querySelector(".gotoTop").addEventListener("click", () => {
+    scrollToPage(document.querySelector("body")) 
+})
